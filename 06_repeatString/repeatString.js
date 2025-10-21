9f8291e55676079ce9b23975e7d37263e2047153
aman <amanrahmanidev@gmail.com>
2025-10-21 15:00:28 +0530

    make function for repeating a string given no of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        let arr = 'ERROR';
+        return arr;
+    }
+    else {
+        let a = string;
+        string = '';
+        for (let i = 0; i < num; i++){
+            string = string + a;
+        }
+    return string;
+    }
 };
+console.log(repeatString('aman', -5));
 
 // Do not edit below this line
 module.exports = repeatString;
