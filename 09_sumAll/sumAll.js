9b34e3ff7c78fe2334687677e55723b83c6b977e
aman <amanrahmanidev@gmail.com>
2025-10-26 17:22:37 +0530

    make function for adding all the no of range a,b

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let total;
+    let initial;
+    if (a > b && typeof a === 'number' && typeof b === 'number'){
+        let c = a;
+        a = b;
+        b = c;
+        total = b; 
+        initial = a;
+        for (let i = 0; i < b - a; i++){
+            total = total + initial;
+            initial++;
+        }
+    }
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0 || a % 1 !== 0 || b % 1 !== 0){
+        return 'ERROR';
+    }
+    if (a < b && typeof a === 'number' && typeof b === 'number') {
+        total = b;
+        initial = a;
+        for (let i = 0; i < b - a; i++){
+            total = total + initial;
+            initial++;
+    }}
+    return total;
 };
+console.log(sumAll(1.6,986));
 
 // Do not edit below this line
 module.exports = sumAll;
