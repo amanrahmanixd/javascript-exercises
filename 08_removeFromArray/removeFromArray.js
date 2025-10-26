7d37de1d87c90f0b6244c670b7ec098efa3dd66c
aman <amanrahmanidev@gmail.com>
2025-10-27 01:14:40 +0530

    make function for removing given numbers from the array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg){
+    let newArray = [];
+    array.forEach((num) => {
+        if (!arg.includes(num)) {
+            newArray.push(num);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
