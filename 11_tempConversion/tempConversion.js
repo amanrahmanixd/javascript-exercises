e4e02ec595408402e6e50960d8cbd03a49acf739
aman <amanrahmanidev@gmail.com>
2025-10-30 20:58:21 +0530

    make a function for coverting temperatures

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let inC = (tempF - 32) * 5/9;
+  let rounded = inC.toFixed(1);
+  let result = parseFloat(rounded);
+  return result;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  inF = tempC * 9/5 + 32;
+  let rounded = inF.toFixed(1);
+  let result = parseFloat(rounded);
+  return result;
 };
 
 // Do not edit below this line
